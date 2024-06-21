3f0d8bdb7a7a50524f8982e2899ce7b41ca5a7a6
eseosapku <75255976+eseosapku@users.noreply.github.com>
2024-06-21 14:30:04 +0200

    commit

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,47 +1,49 @@
 import 'package:flutter/material.dart';
 
 class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(0.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'Save-A-Bite',
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 20),
-            Image.asset(
-              'assets/food_image.jpg',  // Ensure this image is available in your assets folder.
-              height: MediaQuery.of(context).size.height * 0.4,
-              fit: BoxFit.cover,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Welcome to Save-A-Bite',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
               textAlign: TextAlign.center,
             ),
-            SizedBox(height: 10),
-            Text(
+            const SizedBox(height: 5),
+            Image.asset(
+              'assets/welcome_image.jpg',  // Ensure this image is available in your assets folder.
+              height: MediaQuery.of(context).size.height * 0.4,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 0),
+            const Text(
+              'Welcome to Save-A-Bite',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            const Text(
               'Get access to discounted food from local vendors.',
               style: TextStyle(
                 fontSize: 16,
               ),
               textAlign: TextAlign.center,
             ),
-            SizedBox(height: 40),
+            const SizedBox(height: 230),  // Adjust this value as needed to balance the spacing
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
@@ -49,19 +51,19 @@ class Welcome extends StatelessWidget {
                   onPressed: () {
                     // Navigate to Login Screen
                   },
-                  child: Text('Login'),
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.blue,  // Define the color of the button
                   ),
+                  child: const Text('Login'),
                 ),
                 ElevatedButton(
                   onPressed: () {
                     // Navigate to Signup Screen
                   },
-                  child: Text('Signup'),
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.blueAccent,  // Define the color of the button
                   ),
+                  child: const Text('Signup'),
                 ),
               ],
             ),
